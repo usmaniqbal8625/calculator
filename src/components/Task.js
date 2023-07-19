db3cbb622670ded936de5c896c1bca999f8fd259
usmaniqbal8625 <usmaniqbal8625@gmail.com>
2023-07-19 16:08:55 +0500

    calculator

---
+++ src/components/Task.js
@@ -0,0 +1,50 @@
+import React from 'react';
+export default function Task() {
+    return (
+      <>
+   <div className='bg-purple-700 h-96 flex justify-center items-center'>
+    <div className='bg-pink-900 w-72 h-96 rounded-lg '>
+
+
+    <div className=' h-20'>
+      <div className="flex p-3 gap-3">
+    <div className="flex-none w-14 h-14 bg-yellow-600 text-center pt-4 rounded-xl shadow-2xl shadow-black-500/50">C</div>
+    <div className="grow h-14 bg-purple-700 text-right pt-3 pr-2 text-2xl rounded-xl shadow-2xl shadow-black-500/50">0</div>
+    </div>
+
+
+
+<div className='flex flex-wrap gap-3 p-3 h-64 '>
+
+{/* <div className=' '> */}
+    <div className="flex-none w-14 h-14  bg-blue-600 text-center pt-4 rounded-xl shadow-2xl shadow-black-500/50 ">7</div>
+    <div className="flex-none w-14 h-14 bg-blue-600 text-center pt-4 rounded-xl shadow-2xl shadow-black-500/50">8</div>
+    <div className="flex-none w-14 h-14 bg-blue-600 text-center pt-4 rounded-xl shadow-2xl shadow-black-500/50">9</div>
+    <div className="flex-none w-14 h-14 bg-yellow-600 text-center pt-4 rounded-xl shadow-2xl shadow-black-500/50">X</div>
+    <div className="flex-none w-14 h-14 bg-blue-600 text-center pt-4 rounded-xl shadow-2xl shadow-black-500/50">4</div>
+    <div className="flex-none w-14 h-14 bg-blue-600 text-center pt-4 rounded-xl shadow-2xl shadow-black-500/50">5</div>
+    <div className="flex-none w-14 h-14 bg-blue-600 text-center pt-4 rounded-xl shadow-2xl shadow-black-500/50">6</div>
+    <div className="flex-none w-14 h-14 bg-yellow-600 text-center pt-4 rounded-xl shadow-2xl shadow-black-500/50">/</div>
+    <div className="flex-none w-14 h-14 bg-blue-600 text-center pt-4 rounded-xl shadow-2xl shadow-black-500/50">1</div>
+    <div className="flex-none w-14 h-14 bg-blue-600 text-center pt-4 rounded-xl shadow-2xl shadow-black-500/50">2</div>
+    <div className="flex-none w-14 h-14 bg-blue-600 text-center pt-4 rounded-xl shadow-2xl shadow-black-500/50">3</div>
+    <div className="flex-none w-14 h-14 bg-yellow-600 text-center pt-4 rounded-xl shadow-2xl shadow-black-500/50">+</div>
+    <div className="flex-none w-14 h-14 bg-blue-600 text-center pt-4 rounded-xl shadow-2xl shadow-black-500/50">.</div>
+    <div className="flex-none w-14 h-14 bg-blue-600 text-center pt-4 rounded-xl shadow-2xl shadow-black-500/50">0</div>
+    <div className="flex-none w-14 h-14 bg-green-600 text-center pt-4 rounded-xl shadow-2xl shadow-black-500/50">=</div>
+    <div className="flex-none w-14 h-14 bg-yellow-600 text-center pt-4 rounded-xl shadow-2xl shadow-black-500/50">-</div>
+    {/* </div> */}
+</div>
+ 
+   
+    </div>
+   
+    </div>
+    </div>
+      
+   
+  
+  
+       </>
+     )
+  }
